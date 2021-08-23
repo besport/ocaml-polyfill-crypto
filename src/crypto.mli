abe7b465ab5e5743ba86f5be6f3080a8a19c9459
Thibaut <tgudin@besport.com>
2021-08-23 13:11:19 +0200

    adding get_random_values function into Crypto

--- src/crypto.mli
+++ src/crypto.mli
@@ -1,1 +1,19 @@
-(*TODO*)
+[@@@js.stop]
+
+val available : unit -> bool
+
+[@@@js.start]
+
+[@@@js.implem
+let available () =
+  Js_of_ocaml.Js.Optdef.test Js_of_ocaml.Js.Unsafe.global##.crypto]
+
+val get_random_values : int array -> unit
+  [@@js.global "window.crypto.getRandomValues"]
+
+type crypto
+
+val get_crypto : unit -> crypto [@@js.get "window.crypto"]
+
+val get_random_values_v2 : crypto -> int array -> unit
+  [@@js.call "getRandomValues"]
